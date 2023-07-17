be4f1cb06e11a36cb6e92a35ed2cbf93a5380f1f
Esteban Toloza <etoloza@imed.cl>
2023-07-17 15:43:02 -0400

    first commit

---
+++ app/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+const appPort = 3000;
+
+
+
+app.listen(appPort, () => {
+    console.log("adksajksd asd")
+    console.log(`App listening on port ${appPort}`);
+});
